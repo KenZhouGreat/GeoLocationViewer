0a4843a1b538bd14d8a09af129c1a0da921c961c
Ken Zhou <kenzhou.casework@gmail.com>
2014-01-17 17:33:58 +1100

    Initial Commit

---
+++ GeoLocationViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GeoLocationViewer
+//
+//  Created by Ken Zhou on 17/01/2014.
+//  Copyright (c) 2014 Ken Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GeoLocationViewer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GeoLocationViewer
+//
+//  Created by Ken Zhou on 17/01/2014.
+//  Copyright (c) 2014 Ken Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
